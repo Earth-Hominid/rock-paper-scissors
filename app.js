e37ebcc15e60b7686c9a903f0c05b278fe9ace67
C <earthhominid@gmail.com>
2021-04-22 17:35:22 -0300

    added score div

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let playerSelection
 let computerSelection
 var result = playRound()
+let playerCount = 0
+let computerCount = 0
 
 const displayResult = document.getElementById('result')
 const computerChoice = document.getElementById('computer-choice')
@@ -37,16 +39,36 @@ function playRound() {
   if (computerSelection === playerSelection) {
      return result = "It's a tie!"
   } else if (computerSelection === "rock" && playerSelection === "paper") {
-     return result = "you win!"
+     return result = "You win!",
+     playerCount++;
   } else if (computerSelection === "rock" && playerSelection === "scissors") {
-     return result = "you lost";
+     return result = "You lose!",
+     computerCount++;
 }   else if (computerSelection === "paper" && playerSelection === "rock") {
-      return result = "you lost";
+      return result = "You lose!",
+      computerCount++;
 }    else if (computerSelection === "paper" && playerSelection === "scissors") {
-      return result = "you win!"; 
+      return result = "You win!",
+      playerCount++;
 }     else if (computerSelection === "scissors" && playerSelection === "paper") {
-       return result = "you lost";
+       return result = "You lose!",
+       computerCount++;
 }      else if (computerSelection === "scissors" && playerSelection === "rock") {
-        return result = "you win!";
+        return result = "You win!",
+        playerCount++;
   }
 } 
+
+// obtain player choice and play best-of-5 game
+function game () {
+  for (let i = 0; i < 5; i++) {
+        return(playRound(playerSelection, computerSelection))
+  }
+  if (playerScore > compScore) {
+              console.log("You win!");
+        } else {
+              console.log("You have lost!");
+        }
+};
+
+game();
